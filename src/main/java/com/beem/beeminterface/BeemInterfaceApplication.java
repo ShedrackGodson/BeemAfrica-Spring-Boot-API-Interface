91984201b7f326d4c795bddf0d81cbdac0f9c759
ShedrackGodson <hedrickgodson23@gmail.com>
2023-04-26 04:44:26 +0300

    added HELP.md

--- src/main/java/com/beem/beeminterface/BeemInterfaceApplication.java
+++ src/main/java/com/beem/beeminterface/BeemInterfaceApplication.java
@@ -1,13 +1,13 @@
 package com.beem.beeminterface;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
 @SpringBootApplication
 @Slf4j
@@ -17,28 +17,28 @@ public class BeemInterfaceApplication {
         SpringApplication.run(BeemInterfaceApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner run() {
-        return args -> {
-
-            try {
-                SMS sms = new SMS();
-                sms.setSenderId("BIONET");
-                sms.setMessage("This wonderful man! Just from JRE!");
-
-                List<String> recipients = new ArrayList<>();
-                recipients.add("255762547915");
-                recipients.add("255768209424");
-
-                sms.setRecipients(recipients);
-                sms.setScheduleTimeStr(null);
-                Authorize.getEncodedToken();
-
-                sms.sendSms();
-            } catch (Exception e) {
-                throw new IllegalArgumentException("Unexpected exception occurred.");
-            }
-        };
-    }
+    // Uncomment this to test CommandLineRunner
+//    @Bean
+//    CommandLineRunner run() {
+//        return args -> {
+//
+//            try {
+//                SMS sms = new SMS();
+//                sms.setSenderId("INFO");
+//                sms.setMessage("This is wonderful! Just from JRE!");
+//
+//                List<String> recipients = new ArrayList<>();
+//                recipients.add("255762547915");
+//
+//                sms.setRecipients(recipients);
+//                sms.setScheduleTimeStr(null);
+//                Authorize.getEncodedToken();
+//
+//                sms.sendSms();
+//            } catch (Exception e) {
+//                throw new IllegalArgumentException("Unexpected exception occurred.");
+//            }
+//        };
+//    }
 
 }
